350100721698a8d19e2a9749bd93e841e81bd121
Emma Forsberg <emma.forsberg@yaelev.se>
2018-09-18 15:38:35 +0200

    .

--- WeatherNote/Db/WeatherDbContext.cs
+++ WeatherNote/Db/WeatherDbContext.cs
@@ -14,7 +14,7 @@ namespace WeatherNote.Db
         // If you wish to target a different database and/or database provider, modify the 'WeatherDbContext' 
         // connection string in the application configuration file.
         public WeatherDbContext()
-            : base("WeatherDbContext")
+            : base("name=WeatherDbContext")
         {
             Database.SetInitializer(new WeatherDbContextInitializer());
         }
